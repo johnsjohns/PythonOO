940a4a250bf9ede29b8e4e312600cf30cad11a60
johnsjohns <o-ana@hotmail.com>
2022-07-15 21:53:32 -0300

    Adicionado atributos e metodos na classe conta

--- conta.py
+++ conta.py
@@ -1,2 +1,15 @@
 class Conta:
-    pass
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print(self.saldo)
+
+    def saca(self, valor):
+        self.saldo -= valor
+
+    def deposita(self, valor):
+        self.saldo += valor
