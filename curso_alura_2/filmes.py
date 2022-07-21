48bc5a2063f4b836a4cc194ba4da2689afa1ee45
johnsjohns <o-ana@hotmail.com>
2022-07-21 05:52:56 -0300

    Adicionado classes

--- curso_alura_2/filmes.py
+++ curso_alura_2/filmes.py
@@ -8,9 +8,18 @@ class Programa:
     def nome(self):
         return self._nome.title()
 
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+
     @property
     def like(self):
         return self._like
 
     def dar_like(self):
         self._like += 1
+
+class filmes(Programa):
+    def __init__(self, nome, ano, duracao):
+        super.__init__(nome, ano)
+        self._duracao = duracao
