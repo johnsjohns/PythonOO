43722f4e94002fa9d6e3695d441a0f0fa002f80c
johnsjohns <o-ana@hotmail.com>
2022-07-20 22:29:07 -0300

    Finalizado curso

--- conta.py
+++ conta.py
@@ -8,8 +8,15 @@ class Conta:
     def extrato(self):
         print(self.__saldo)
 
+    def __pode_sacar(self, valor_a_sacar):
+        valor_limite = self.__saldo + self.__limite
+        return valor_a_sacar <= valor_limite
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if(self.__pode_sacar(valor)):
+            self.__saldo -= valor
+        else:
+            print("Saldo insuficiente!")
 
     def deposita(self, valor):
         self.__saldo += valor
@@ -17,15 +24,26 @@ class Conta:
     def tranfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
-
-    def get_saldo(self):
+    @property
+    def saldo(self):
         return self.__saldo
 
-    def get_titular(self):
+    @property
+    def titular(self):
         return self.__titular
 
-    def get_limite(self):
+    @property
+    def limite(self):
         return self.__limite
 
-    def set_limite(self, limite):
+    @limite.setter
+    def limite(self, limite):
         self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
+
+    @staticmethod
+    def codigos_bancos():
+        return {'BB': '001', 'Caixa': '104', 'Bradesco': "237"}
