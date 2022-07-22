c82e21979b424de5514b45f5d736fb6b34b73e50
johnsjohns <o-ana@hotmail.com>
2022-07-22 05:02:35 -0300

    Feito exercicio de herança e polimorfia

--- curso_alura_2/filmes.py
+++ curso_alura_2/filmes.py
@@ -1,25 +1,89 @@
 class Programa:
     def __init__(self, nome, ano):
-        self._nome = nome
-        self._ano = ano
-        self._like = 0
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    def dar_likes(self):
+        self._likes += 1
 
     @property
     def nome(self):
-        return self._nome.title()
+        return self._nome
 
     @nome.setter
     def nome(self, nome):
         self._nome = nome
 
-    @property
-    def like(self):
-        return self._like
+    def __str__(self):
+        return f'Nome: {self._nome} - Likes: {self._likes}'
 
-    def dar_like(self):
-        self._like += 1
-
-class filmes(Programa):
+class Filme(Programa):
     def __init__(self, nome, ano, duracao):
-        super.__init__(nome, ano)
-        self._duracao = duracao
+        super().__init__(nome, ano)
+        self.duracao = duracao
+    def __str__(self):
+        return f'Nome: {self._nome} - {self.duracao} min -Likes: {self._likes}'
+
+
+class Serie(Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+    def __str__(self):
+        return f'Nome: {self._nome} - {self.temporadas} temporadas - Likes: {self._likes}'
+
+class Playlist():
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __len__(self):
+        return len(self._programas)
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+
+
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+demolidor = Serie("demolidor", 2016, 2)
+tmep = Filme('todo mundo em pânico', 1998, 120)
+
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+vingadores.dar_likes()
+
+atlanta.dar_likes()
+atlanta.dar_likes()
+atlanta.dar_likes()
+
+tmep.dar_likes()
+tmep.dar_likes()
+
+
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+demolidor.dar_likes()
+
+lista = [atlanta, vingadores, demolidor, tmep]
+minha_playlist = Playlist('fim de semana', lista)
+print(f'Tamanho da Playlist: {len(minha_playlist)}')
+for programa in minha_playlist.listagem:
+    print(programa)
